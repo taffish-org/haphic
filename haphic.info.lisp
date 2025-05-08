db0085ac089a4f9af0e9dbe8c0057b1cef6ed455
Kaiyuan Han <hermit_han@163.com>
2025-05-08 21:40:36 +0800

    add /HapHiC/utils/... cmds

--- haphic.info.lisp
+++ haphic.info.lisp
@@ -4,4 +4,20 @@
 (:version        "1.0.6")
 (:intro          "HapHiC: a fast, reference-independent, allele-aware scaffolding tool based on Hi-C data")
 (:dependencies)
-(:functions      "haphic")
+(:functions      "haphic"
+                 "agp_to_fasta"
+                 "juicer"
+                 "filter_bam"
+                 "combine_groups.py"
+                 "convert_gfa_ids.py"
+                 "fasta_count_N.py"
+                 "fastq_length_filtering.py"
+                 "filter_bam.py"
+                 "find_telomeres.py"
+                 "gfa_depth_to_bedGraph.py"
+                 "groups_to_clusters.py"
+                 "mock_agp_file.py"
+                 "mock_blast_file.py"
+                 "prepare_clusters.py"
+                 "remove_singletons.py"
+                 "reverse_bed.py")
